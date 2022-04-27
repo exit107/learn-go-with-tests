619a2c73742446172f172179d1ff9c396a927afe
Dave Brown <dave.brown@exit107.com>
2022-04-26 22:29:50 -0600

    Add default name if none is provided

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	want := "Hello, Dave"
-	got := Hello("Dave")
+	t.Run("say 'Hello, Dave' when 'Dave' is passed as a string", func(t *testing.T) {
+		want := "Hello, Dave"
+		got := Hello("Dave")
 
-	if want != got {
-		t.Errorf("wanted: %q, got: %q", want, got)
-	}
+		if want != got {
+			t.Errorf("wanted: %q, got: %q", want, got)
+		}
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		want := "Hello, World"
+		got := Hello("")
+
+		if want != got {
+			t.Errorf("wanted: %q, got: %q", want, got)
+		}
+	})
 }
