ca62091ed2e9358e7fa3dffb1e69ea15544ec57f
Dave Brown <dave.brown@exit107.com>
2022-04-26 22:32:11 -0600

    Refactor test to move assertion into function

--- hello_test.go
+++ hello_test.go
@@ -3,21 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	t.Run("say 'Hello, Dave' when 'Dave' is passed as a string", func(t *testing.T) {
-		want := "Hello, Dave"
-		got := Hello("Dave")
-
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
 		if want != got {
 			t.Errorf("wanted: %q, got: %q", want, got)
 		}
+	}
+
+	t.Run("say 'Hello, Dave' when 'Dave' is passed as a string", func(t *testing.T) {
+		want := "Hello, Dave"
+		got := Hello("Dave")
+		assertCorrectMessage(t, got, want)
+
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		want := "Hello, World"
 		got := Hello("")
-
-		if want != got {
-			t.Errorf("wanted: %q, got: %q", want, got)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 }
